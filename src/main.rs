d18891faf50b3a411941c25008be51af8fcdc22c
Gabriel Sanches <gabriel@gsr.dev>
2020-11-20 19:19:42 -0300

    main: Run app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+mod app;
 mod client;
+mod utils;
 
-fn main() {}
+use self::app::{App, Output};
+use clap::Clap;
+use std::io::{self, Result, Write};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+	let app = App::parse();
+	let mut output = Output {
+		stdout: io::stdout(),
+		stderr: io::stderr(),
+	};
+
+	let result = app.run(&mut output).await;
+
+	if let Err(err) = result {
+		return writeln!(output.stderr, "{}: {}", utils::get_name(), err);
+	}
+
+	Ok(())
+}
